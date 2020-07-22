970875f5c742bccea4a6fc64bc4121dc8d657c47
Priyanka <mail2priyankanayak@gmail.com>
2020-07-22 22:24:27 +0530

    Assignment

---
+++ src/CheckInputChar.java
@@ -0,0 +1,9 @@
+/*Q.5 WAP in java to check the input characters for uppercase, lowercase, number of digits and
+
+        other characters.*/
+public class CheckInputChar {
+    public static void main(String[] args){
+
+
+    }
+}

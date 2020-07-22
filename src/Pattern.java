1e68da6a4408798b872fa9630314d3c982c255af
Priyanka <mail2priyankanayak@gmail.com>
2020-07-22 22:25:22 +0530

    Assignment

--- src/Pattern.java
+++ src/Pattern.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+/* Write a program to print the following pattern given n as argument: - for input 3
+
+1
+
+2 2
+
+3 3 3
+*/
 
 public class Pattern {
+    public static void main(String[] args){
+
+        for(int i=1;i<=3;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println(" ");
+
+        }
+
+    }
 }
